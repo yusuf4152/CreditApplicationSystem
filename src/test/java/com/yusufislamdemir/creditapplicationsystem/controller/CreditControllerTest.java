8d18af5fa9ca946675660624c5ca9c3b30f60afd
yusuf islam demir <yusufislamdemir41@gmail.com>
2023-02-20 18:48:19 +0300

    added active profiles in CreditControllerTest.java

--- src/test/java/com/yusufislamdemir/creditapplicationsystem/controller/CreditControllerTest.java
+++ src/test/java/com/yusufislamdemir/creditapplicationsystem/controller/CreditControllerTest.java
@@ -31,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class CreditControllerTest {
 
     @MockBean
